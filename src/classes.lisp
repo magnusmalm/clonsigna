ee403c033f96ebea1d94d0ce6a24e3b32dcb4e0e
Andrea Chiumenti <kiuma72@gmail.com>
2012-02-16 12:09:30 +0100

    Removed format (format) varibale to allow compiler optimization

--- src/classes.lisp
+++ src/classes.lisp
@@ -130,12 +130,11 @@ It's usually instantiated by the MAKE-IMAP function and activated by CMD-CONNECT
 (defmethod imap-socket-send-command ((is imap-socket) command &rest args)
   (imap-socket-flush-buffer is)
   (let ((s (imap-socket-socket is))
-        (line-term (format nil "~a~a" #\Return #\Linefeed))
-        (format-string "~a ~a~{ ~a~}~a"))
+        (line-term (format nil "~a~a" #\Return #\Linefeed)))
     (imap-socket-next-message is)
     (when +debug+
-      (format t format-string (imap-socket-message-id is) (symbol-name command)  args line-term))
-    (format s format-string (imap-socket-message-id is) (symbol-name command)  args line-term)
+      (format t "~a ~a~{ ~a~}~a" (imap-socket-message-id is) (symbol-name command)  args line-term))
+    (format s "~a ~a~{ ~a~}~a" (imap-socket-message-id is) (symbol-name command)  args line-term)
     (finish-output s)))
 
 (defmethod imap-socket-has-capability ((is imap-socket) capability)
